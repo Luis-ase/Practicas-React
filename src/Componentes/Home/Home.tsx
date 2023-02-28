9613554810abeb5e86a5067c5ba3e6aaa5edb776
Michael Luis Condori Paye <luis2003nb@gmail.com>
2023-02-28 13:00:12 -0300

    first

---
+++ src/Componentes/Home/Home.tsx
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+function home() {
+    return (
+      <div >
+        <h2 className="text-3xl font-bold underline">HEllo</h2>
+        
+      </div>
+    );
+  }
+  
+  export default home;
---
+++ src/Componentes/Project/Bateria-1/Bateria.tsx
@@ -0,0 +1,13 @@
+
+
+
+function Bateri() {
+    return (
+      <div >
+        <h3 className="text-3xl font-bold underline">Bateria</h3>
+        
+      </div>
+    );
+  }
+  
+  export default Bateri;
