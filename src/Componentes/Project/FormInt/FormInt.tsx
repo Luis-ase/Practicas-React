0c5fdea0d7e623f69055c67b4b23710a05e9ad6a
Michael Luis Condori Paye <luis2003nb@gmail.com>
2023-02-28 20:07:12 -0300

    Add file desafio front-Mentor(Formulario-interactivo)

---
+++ src/Componentes/Project/FormInt/FormInt.tsx
@@ -0,0 +1,13 @@
+import Cardfrontal from "./images/bg-card-front.png"
+
+function FormInt(){
+    
+    
+    return(
+        <div>
+            <img src={Cardfrontal} alt="Card Frontal" />
+        </div>
+    )
+}
+
+export default FormInt;
