4988941427672169b7b2a18e65502b21fab29368
Michael Luis Condori Paye <luis2003nb@gmail.com>
2023-04-18 10:15:13 -0300

    Practicando Css

--- src/Componentes/Project/CssSmallProjects/CssSmallProjects.tsx
+++ src/Componentes/Project/CssSmallProjects/CssSmallProjects.tsx
@@ -3,9 +3,18 @@ import React from "react"
 const CssSmallProjects = () => {
     return (
         <main>
-            <div className="w-screen h-screen bg-pistola bg-cover">
+            {/* combinar texto con el fondo  */}
+            {/* <div className="w-screen h-screen bg-pistola bg-cover">
                 <h1 className="text-7xl leading-[100vh] text-center uppercase mix-blend-overlay text-white " >Bienvenido a mi pagina web</h1>
-            </div>
+            </div> */}
+            {/* galeria fondos */}
+            {/* <section className="w-full h-screen bg-astronauota bg-cover bg-center bg-fixed"></section>
+            <div className="w-full h-20"></div>
+            <section className="w-full h-screen bg-astronauota2 bg-cover bg-center bg-fixed"></section>
+            <div className="w-full h-20"></div>
+            <section className="w-full h-screen bg-astronauota3 bg-cover bg-center bg-fixed "></section>
+            <div className="w-full h-20"></div>
+            <section className="w-full h-screen bg-astronauota4 bg-cover bg-center bg-fixed"></section> */}
         </main>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,11 @@ module.exports = {
      },
      backgroundImage:{
       pistola:"url(https://images.unsplash.com/photo-1595590424283-b8f17842773f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80)",
-     }
+      astronauota:"url(https://pbs.twimg.com/media/Ft3BThQXwAILIHP?format=png&name=small)",
+      astronauota2:"url(https://pbs.twimg.com/media/Ft3Ba7hX0AEShFV?format=png&name=small)",
+      astronauota3:"url(https://pbs.twimg.com/media/Ft3CfvhX0AIpR83?format=png&name=small)",
+      astronauota4:"url(https://pbs.twimg.com/media/Ft3FppmWYAEThQ4?format=png&name=small)",
+    }
     },
   },
   plugins: [],
