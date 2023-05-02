e0d4d6dfc2a3f8dd606f2e416f13b0dfdf24a9be
Michael Luis Condori Paye <luis2003nb@gmail.com>
2023-05-01 21:40:06 -0300

    new proyect cssPractic- perdon por comentar pero tengo que seguir practicando

--- src/Componentes/Project/CssSmallProjects/CssSmallProjects.tsx
+++ src/Componentes/Project/CssSmallProjects/CssSmallProjects.tsx
@@ -1,5 +1,6 @@
 import React from "react"
-
+import { } from "react-icons/ai"
+import { Link } from "react-router-dom"
 const CssSmallProjects = () => {
     return (
         <main>
@@ -15,6 +16,94 @@ const CssSmallProjects = () => {
             <section className="w-full h-screen bg-astronauota3 bg-cover bg-center bg-fixed "></section>
             <div className="w-full h-20"></div>
             <section className="w-full h-screen bg-astronauota4 bg-cover bg-center bg-fixed"></section> */}
+            <div>
+
+            </div>
+            {/* <header>
+                <nav className="bg-red-500 h-[4rem] top-0 sticky w-screen">
+                    <div className="w-screen mx-auto my-0 flex items-center justify-center">
+                        <Link className="text-gray-800 text-xl uppercase leading-[50px] px-3 py-3" to="#hola">inicio</Link>
+                        <Link className="text-gray-800 text-xl uppercase leading-[50px] px-3 py-3" to="#hola">Servicios</Link>
+                        <Link className="text-gray-800 text-xl uppercase leading-[50px] px-3 py-3" to="#hola">Precios</Link>
+                        <Link className="text-gray-800 text-xl uppercase leading-[50px] px-3 py-3" to="#hola">Compañia</Link>
+                        <Link className="text-gray-800 text-xl uppercase leading-[50px] px-3 py-3" to="#hola">Soporte</Link>
+
+                    </div>
+                </nav>
+                <div className="h-screen w-screen ">
+                    <img className="w-full h-[100%] object-cover object-center" src="https://pbs.twimg.com/media/Ft3BThQXwAILIHP?format=png&name=small" alt="astronau" />
+                </div>
+            </header> */}
+            
+            {/* <div className="h-[500px] w-screen"> */}
+                {/* <div className="w-full relative h-full "> */}
+
+                    {/* <aside className="w-[15%] m-4 bg-gray-800 p-4 rounded-lg text-center sticky"> */}
+                    
+{/*                    
+                        <h3 className="text-2xl font-bold text-gray-400 ">
+                            Temario
+                        </h3>
+                        <nav>
+                            <Link to="#algunlado" className="block text-center text-red-500 ">Articulo 1</Link>
+                            <Link to="#algunlado" className="block text-center text-red-500 ">Articulo 2</Link>
+                            <Link to="#algunlado" className="block text-center text-red-500 ">Articulo 3</Link>
+                            <Link to="#algunlado" className="block text-center text-red-500 ">Articulo 4</Link>
+                        </nav> */}
+                    {/* </aside> */}
+                    
+                 {/*                  
+                    <div className=" absolute top-0 left-[20%]">
+                        <div>
+                            <h2 className="text-2xl font-bold ">Articulo 1</h2>
+                            <p className="mb-2 leading-2 text-gray-700 font-bold">Documentacion</p>
+                            <p className="mb-2 leading-2">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Ipsum soluta dolor officiis obcaecati tenetur id incidunt,
+                                ullam dolores deserunt assumenda cupiditate culpa tempora,
+                                minus quibusdam ducimus cumque. Veritatis, corrupti eos!
+                            </p>
+                        </div>
+                        <div>
+                            <h2 className="text-2xl font-bold ">Articulo 2</h2>
+                            <p className="mb-2 leading-2 text-gray-700 font-bold">Documentacion</p>
+                            <p className="mb-2 leading-2">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Ipsum soluta dolor officiis obcaecati tenetur id incidunt,
+                                ullam dolores deserunt assumenda cupiditate culpa tempora,
+                                minus quibusdam ducimus cumque. Veritatis, corrupti eos!
+                            </p>
+                        </div>
+                        <div>
+                            <h2 className="text-2xl font-bold ">Articulo 3</h2>
+                            <p className="mb-2 leading-2 text-gray-700 font-bold">Documentacion</p>
+                            <p className="mb-2 leading-2">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Ipsum soluta dolor officiis obcaecati tenetur id incidunt,
+                                ullam dolores deserunt assumenda cupiditate culpa tempora,
+                                minus quibusdam ducimus cumque. Veritatis, corrupti eos!
+                            </p>
+                        </div>
+                        <div>
+                            <h2 className="text-2xl font-bold ">Articulo 4</h2>
+                            <p className="mb-2 leading-2 text-gray-700 font-bold">Documentacion</p>
+                            <p className="mb-2 leading-2">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Ipsum soluta dolor officiis obcaecati tenetur id incidunt,
+                                ullam dolores deserunt assumenda cupiditate culpa tempora,
+                                minus quibusdam ducimus cumque. Veritatis, corrupti eos!
+                            </p>
+                        </div>
+                    </div>
+                    */}
+                   
+                {/* </div>  */}
+            {/*                 
+                <div>
+                    <img  className="w-full h-[auto] object-cover object-center" src="https://pbs.twimg.com/media/Ft3Ba7hX0AEShFV?format=png&name=small" alt="astronaut2" />
+                </div> */}
+            {/* </div> */}
+                     {/* </div> */}
         </main>
     )
 }
